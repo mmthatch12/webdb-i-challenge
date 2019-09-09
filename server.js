396f0cff98f3562f1787e715d04f2c148e791b3f
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-09 14:22:09 -0600

    first get completed for the accounts db

--- server.js
+++ server.js
@@ -10,4 +10,14 @@ server.get('/', (req, res) => {
     res.send('Sanity Check')
 })
 
+server.get('/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(error => {
+            res.json({ error: 'Could not load accounts'})
+        })
+})
+
 module.exports = server;
